a026df053c2aa07f8cb6204d3c5ff28ad2d821a6
dontroll17 <dontroll@yandex.ru>
2022-06-21 16:23:22 +0300

    fix: fix var name

--- ws-server/screen.ts
+++ ws-server/screen.ts
@@ -5,7 +5,7 @@ export const getScreen = async () => {
     let {x, y} = robot.getMousePos();
     let img = robot.screen.capture(x - 100, y - 100, 200, 200).image;
     const pic = new Jimp({data: img, width: 200, height: 200})
-    const base = await pic.getBase64Async(pic.getMIME());
-    const str = base.slice(22);
-    return str;
+    const getBase = await pic.getBase64Async(pic.getMIME());
+    const base64str = getBase.slice(22);
+    return base64str;
 }
